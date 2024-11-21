dbe2cec0d1059880a78f6d42e67d8ba016c85cac
NeyleCM <neylecmorantes@gmail.com>
2024-11-21 10:09:25 +0100

    'conectada-con-front'

--- routes/tasks.js
+++ routes/tasks.js
@@ -1,13 +1,13 @@
 const express = require('express')
 const router = express.Router()
-const Task = require('../models/Task.js')
+//const Task = require('../models/Task.js')
 const TaskController = require('../controllers/TaskController.js')
 
 router.post("/create", TaskController.create)
 router.get('/', TaskController.getAll )
 router.get('/ssr', TaskController.getAllSSR )
 router.get('/id/:_id', TaskController.getByID)
-router.put("/markascompleted/:_id", TaskController.updateCompleted)
+router.put("/mark-as-completed/:_id", TaskController.updateCompleted) //Corregí nombre con '-'
 router.put('/id/:_id', TaskController.updateByName) 
 router.delete('/id/:_id', TaskController.deleteTask )
 
